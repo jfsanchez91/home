e2cb729c39b6a8e4f68b0bb89b35abf20e8906fd
Jorge F. Sanchez <mail@jfsanchez.net>
2025-05-09 11:11:53 +0200

    Force cleaning local files when using clone script

--- .local/bin/.home_setup/clone.sh
+++ .local/bin/.home_setup/clone.sh
@@ -5,6 +5,7 @@ git init --bare $HOME/.home.git
 home="git --work-tree $HOME --git-dir=$HOME/.home.git"
 $home remote add origin git@github.com:jfsanchez91/home.git
 $home fetch origin
+$home clean -fd
 $home checkout -b master --track origin/master
 $home reset origin/master
 echo "Done"
