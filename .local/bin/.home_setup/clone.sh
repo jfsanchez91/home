26986ec27a30b9c988d4af409cbcba5d42161839
Jorge F. Sanchez <mail@jfsanchez.net>
2025-05-09 11:06:03 +0200

    Fix home alias in clone script

--- .local/bin/.home_setup/clone.sh
+++ .local/bin/.home_setup/clone.sh
@@ -2,9 +2,9 @@
 
 echo "Setting up home clone"
 git init --bare $HOME/.home.git
-alias home="git --work-tree $HOME --git-dir=$HOME/.home.git"
-home remote add origin git@github.com:jfsanchez91/home.git
-home fetch origin
-home checkout -b master --track origin/master
-home reset origin/master
+home="git --work-tree $HOME --git-dir=$HOME/.home.git"
+$home remote add origin git@github.com:jfsanchez91/home.git
+$home fetch origin
+$home checkout -b master --track origin/master
+$home reset origin/master
 echo "Done"
