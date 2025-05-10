a32befd7fbb56926cb532b2154444c2d7827f586
Jorge F. Sánchez <3751757+jfsanchez91@users.noreply.github.com>
2025-05-10 18:54:53 +0200

    Remove sdkman installation from base arch

--- .local/bin/.home_setup/arch.sh
+++ .local/bin/.home_setup/arch.sh
@@ -30,8 +30,3 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 echo "Installing powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
-
-echo "Installing SDKMan"
-curl -s "https://get.sdkman.io" | bash
-
