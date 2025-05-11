5a759ba70bc8eb910d4350963d071ea23bd1744d
Jorge F. Sanchez <mail@jfsanchez.net>
2025-05-11 21:15:40 +0200

    fix clone setup script and add jetbrains-toolbox to hyprland starting

--- .local/bin/.home_setup/clone.sh
+++ .local/bin/.home_setup/clone.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 echo "Setting up home clone"
-git init --bare $HOME/.home.git
-home="git --work-tree $HOME --git-dir=$HOME/.home.git"
+
+# Check if .home.git already exists
+if [ ! -d "$HOME/.home.git" ]; then
+    echo "Initialize bare git repository"
+    git init --bare "$HOME/.home.git"
+fi
+
+
+echo "Define git command with home work-tree and git-dir"
+home="git --work-tree=$HOME --git-dir=$HOME/.home.git"
+
+echo "Add remote and fetch"
 $home remote add origin https://github.com/jfsanchez91/home.git
 $home fetch origin
-$home checkout -b master --track origin/master --merge
-if [ $? -ne 0 ]; then
-    echo "Conflicts detected. Removing conflicting files..."
-    $home status --porcelain | grep '^UU' | awk '{print $2}' | xargs rm -f
-    $home checkout --ours .
-    $home reset origin/master
+
+echo "Try to checkout master, force overwrite any existing files"
+if ! $home checkout -f master --track origin/master; then
+    echo "Conflicts detected. Forcing update..."
+    echo "Remove any conflicting files"
+    $home status --porcelain | grep '^UU' | awk '{print $2}' | xargs -r rm -f
+    echo "Force checkout of remote files"
+    $home checkout -f master
+    echo "Reset to match origin/master"
+    $home reset --hard origin/master
 fi
 echo "Done"
 
