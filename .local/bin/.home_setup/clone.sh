d2b0d6aedf4d055c17dbcf8c178cf307d58fad28
Jorge F. Sanchez <mail@jfsanchez.net>
2025-05-11 10:48:54 +0200

    Fix clone script to avoid removing untracked files

--- .local/bin/.home_setup/clone.sh
+++ .local/bin/.home_setup/clone.sh
@@ -5,7 +5,12 @@ git init --bare $HOME/.home.git
 home="git --work-tree $HOME --git-dir=$HOME/.home.git"
 $home remote add origin git@github.com:jfsanchez91/home.git
 $home fetch origin
-$home clean -fd
-$home checkout -b master --track origin/master
-$home reset origin/master
+$home checkout -b master --track origin/master --merge
+if [ $? -ne 0 ]; then
+    echo "Conflicts detected. Removing conflicting files..."
+    $home status --porcelain | grep '^UU' | awk '{print $2}' | xargs rm -f
+    $home checkout --ours .
+    $home reset origin/master
+fi
 echo "Done"
+
