738e1c499aa0b83602c14233aa888c30967fbedb
Jorge F. Sanchez <mail@jfsanchez.net>
2025-05-11 21:24:28 +0200

    dev: add docker config

--- .local/bin/.home_setup/arch_dev.sh
+++ .local/bin/.home_setup/arch_dev.sh
@@ -3,9 +3,15 @@
 echo "Installing SDKMan"
 curl -s "https://get.sdkman.io" | bash
 
-echo "Installing JetBrains ToolBox"
+echo "Installing"
 paru -Su --noconfirm --needed \
 	jetbrains-toolbox \
 	microsoft-edge-stable-bin \
-	visual-studio-code-bin
+	visual-studio-code-bin \
+	docker \
+	docker-compose
+
+echo "Setting up docker"
+sudo usermod -a -G docker $USER
+newgrp docker
 
